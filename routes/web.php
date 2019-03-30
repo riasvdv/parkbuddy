1b22145a5933d272077bef853f250e2b6c094691
Rias <hello@rias.be>
2019-03-30 15:26:30 +0100

    Route cache

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::match(['get', 'post'], '/botman', 'BotManController@handle');
-Route::get('/botman/tinker', 'BotManController@tinker');
